844603254d64894841a07135fc226a15ac938e44
amar.lakshya <amar.lakshya@xaviers.edu.in>
2019-10-11 22:17:16 +0100

    just checking

--- assignments/OS/exercise1/sort_simple/sort.c
+++ assignments/OS/exercise1/sort_simple/sort.c
@@ -1,17 +1,46 @@
 #include <stdio.h>
-int get_input(int fd)
+
+const int LINES = 20;
+const int ROWS = 20;
+
+typedef struct {
+    char array[20][20];
+} Text;
+
+void print_input(Text input)
 {
-    while
-	return 0;
+    printf("output:\n");
+    for (int i = 0; i < 20; i++) {
+	for (int j = 0; j < 20; j++) {
+	    if (input.array[i][j] != '\0') {
+		printf("%c", input.array[i][j]);
+	    }
+	}
+    }
+}
+
+Text get_input(Text input)
+{
+    /** char input[20][20] = { '\0' }; */
+    int index = 0, line = 0;
+    char c;
+    while (line < 20) {
+	while ((c = getchar()) != '\n' && index <= 20) {
+	    input.array[line][index] = c;
+	    index = index + 1;
+	}
+	if (c == '\n') {
+	    input.array[line][index] = '\n';
+	    index = 0;
+	}
+	line++;
+    }
+
+    return input;
 }
 int main(int argc, char** argv)
 {
-    int ret = 1;
-    int fd;
-    if (argc > 1) {
-	ret = 2;
-    } else {
-	ret = get_input(argv);
-    }
-    return ret;
+    Text input = { '\0' };
+    print_input(get_input(input));
+    return 0;
 }
