9afabab185ea999a4b676061d6932eed8407a7d6
amar.lakshya <axx945@cs.bham.ac.uk>
2019-11-03 15:34:20 +0000

    REMOVED: comments

--- assignments/CRYPTO/code/ex2/sha1.py
+++ assignments/CRYPTO/code/ex2/sha1.py
@@ -1,6 +1,8 @@
 import hashlib
+import time
+import random
 
-SIZE = 2
+SIZE = 10
 
 
 def getHash(x):
@@ -10,51 +12,23 @@ def getHash(x):
 
 
 def floyd(f, x0):
-    # Main phase of algorithm: finding a repetition x_i = x_2i.
-    # The hare moves twice as quickly as the tortoise and
-    # the distance between them increases by 1 at each step.
-    # Eventually they will both be inside the cycle and then,
-    # at some point, the distance between them will be
-    # divisible by the period λ.
-    tortoise = f(f(x0))  # f(x0) is the element/node next to x0.
-    hare = f(f(f(f(x0))))
-    print("tortoise, hare: {},{}\n".format(tortoise, hare))
+    tortoise = f(x0)  # f(x0) is the element/node next to x0.
+    hare = f(f(x0))
     c = 0
     while tortoise != hare:
-        tortoise = f(f(tortoise))
-        hare = f(f(f(f(hare))))
-        print("tortoise, hare: {},{}\n".format(tortoise, hare))
+        tortoise = f(tortoise)
+        hare = f(f(hare))
         c += 1
-    return
-    # At this point the tortoise position, ν, which is also equal
-    # to the distance between hare and tortoise, is divisible by
-    # the period λ. So hare moving in circle one step at a time,
-    # and tortoise (reset to x0) moving towards the circle, will
-    # intersect at the beginning of the circle. Because the
-    # distance between them is constant at 2ν, a multiple of λ,
-    # they will agree as soon as the tortoise reaches index μ.
-
-    # Find the position μ of first repetition.
-    mu = 0
     tortoise = x0
     while tortoise != hare:
-        tortoise = f(f(tortoise))
-        hare = f(f(hare))   # Hare and tortoise move at same speed
-        print("tortoise, hare: {},{}".format(tortoise, hare))
-        mu += 1
-    print("Index: {}, {}".format(c, mu))
-
-    # Find the length of the shortest cycle starting from x_μ
-    # The hare moves one step at a time while tortoise is still.
-    # lam is incremented until λ is found.
-#      lam = 1
-    #  hare = f(tortoise)
-    #  while tortoise != hare:
-    #  hare = f(hare)
-    #  lam += 1
-#
-    #  return lam, mu
-#
+        last_x = tortoise
+        last_y = hare
+        tortoise = f(tortoise)
+        hare = f(hare)   # Hare and tortoise move at same speed
+    return last_x, last_y, f(last_x)
 
 
-print(floyd(getHash, "A"))
+start = time.time()
+print(floyd(getHash, str(random.random())))
+end = time.time()
+print("Time taken: {}".format(end - start))
