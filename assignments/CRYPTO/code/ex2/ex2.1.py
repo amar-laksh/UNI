2e88a2f7d5040794c0bb029e28ab93298d28fa3f
amar.lakshya <axx945@cs.bham.ac.uk>
2019-11-03 12:27:40 +0000

    CRYPTO: ex2 almost done?

--- assignments/CRYPTO/code/ex2/ex2.1.py
+++ assignments/CRYPTO/code/ex2/ex2.1.py
@@ -6,19 +6,18 @@
 # Last Modified Date: 09.10.2019
 # Last Modified By  : Amar Lakshya <amar.lakshya@xaviers.edu.in>
 import sys
-sys.path.insert(1, 'AES')
+sys.path.insert(1, 'Python-AES')
 from AES import *
 
 plaintext = "00000000000000000000000000000000"
 key = "00000000000000000000000000000000"
-#  key = "000102030405060708090a0b0c0d0e0f"
 key = process_key(key, 4)
-expanded_key = KeyExpansion(key, 4, 4, 0)
-print("PLAINTEXT:\t {0}".format(plaintext))
-
+Nr = 0
+expanded_key = KeyExpansion(key, 4, 4, Nr)
 block = process_key(plaintext)
-block = Cipher(block, expanded_key, 4, 4, 0)
+block = Cipher(block, expanded_key, 4, 4, Nr)
+print("PLAINTEXT:\t {0}".format(plaintext))
 print("ENCRYPTED:\t {0}".format(str_block_line(block)))
 
-block = InvCipher(block, expanded_key, 4, 4, 0)
+block = InvCipher(block, key, 4, 4, Nr)
 print("DECRYPTED:\t {0}".format(str_block_line(block)))
--- assignments/CRYPTO/code/ex2/ex2.2.py
+++ assignments/CRYPTO/code/ex2/ex2.2.py
@@ -6,21 +6,27 @@
 # Last Modified Date: 09.10.2019
 # Last Modified By  : Amar Lakshya <amar.lakshya@xaviers.edu.in>
 import sys
-sys.path.insert(1, 'AES')
+sys.path.insert(1, 'Python-AES')
 from AES import *
 
 plaintext = "00000000000000000000000000000000"
-key = "11111111111111111111111111111111"
-#  key = "00000000000000000000000000000000"
-#  key = "01235564928342342352342342523423"
+key = "ffffffffffffffffffffffffffffffff"
 key = process_key(key, 4)
-Nr = 0
+Nr = 1
 expanded_key = KeyExpansion(key, 4, 4, Nr)
 
-print("key:\t {0}".format(key))
-print("PLAINTEXT:\t {0}".format(plaintext))
 
 block = process_key(plaintext)
+
+#  Printing out subkeys
+print("key:\t {0}".format(str_block_line(expanded_key)))
+c = 0
+for i in range(0, (32 * Nr) + 1, 32):
+    print("k{}:\t{}".format(c, str_block_line(expanded_key)[i:i+32]))
+    c += 1
+print("PLAINTEXT:\t {0}".format(plaintext))
+
+
 block = Cipher(block, expanded_key, 4, 4, Nr)
 print("ENCRYPTED:\t {0}".format(str_block_line(block)))
 
