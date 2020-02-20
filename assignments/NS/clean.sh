ef711e4af9464147900a6dc080d57dc4c1cc0c2c
amar.lakshya <axx945@cs.bham.ac.uk>
2020-02-20 18:21:17 +0000

    INITIAL NETWORK WORKING

---
+++ assignments/NS/clean.sh
@@ -0,0 +1,1 @@
+sudo docker stop client server router ;sudo docker rm client server router; sudo docker network rm server-net client-net
--- assignments/NS/firewall.sh
+++ assignments/NS/firewall.sh
@@ -25,21 +25,20 @@ set -o nounset                                  # Treat unset variables as an er
 #sudo docker build --no-cache -t ubuntu .
 
 # Creating server-net and client-net networks
-#sudo docker network create --driver=bridge --subnet=192.168.100.0/24  client-net && \
-#sudo docker network create --driver=bridge --subnet=192.168.101.0/24  server-net && \
-#
-# Attaching containers to the networks
-sudo docker run --network=client-net --ip 192.168.100.2 -itd --name=client ubuntu /bin/bash && \
-sudo docker run --network=server-net --ip 192.168.101.2 -itd --name=server ubuntu /bin/bash && \
-# Creating and attaching router container to both server-net and client-net networks
-sudo docker run --network=server-net --ip 192.168.101.1 -itd --name=router ubuntu /bin/bash && \
-exit 0 && \
-sudo docker create --network=server-net --ip 192.168.101.1 -it --name=router ubuntu /bin/bash && \
-sudo docker create --network=server-net --ip 192.168.101.1 -it --name=router ubuntu /bin/bash && \
-sudo docker network connect --ip 192.168.100.1 client-net router && \
+sudo docker network create --driver=bridge --subnet=192.168.100.0/24 --ip-range=192.168.100.1/24  client-net && \
+sudo docker network create --driver=bridge --subnet=192.168.101.0/24 --ip-range=192.168.101.1/24  server-net && \
 
+# Attaching containers to the networks
+sudo docker run -itd --network=client-net --name=client ubuntu /bin/bash && \
+sudo docker run -itd --network=server-net --name=server ubuntu /bin/bash && \
+
+# Creating and attaching router container to both server-net and client-net networks
+#sudo docker run -itd --network=server-net --name=router ubuntu /bin/bash && \
+sudo docker create -it --network=server-net  --name=router ubuntu /bin/bash && \
+sudo docker network connect client-net router && \
+sudo docker start router && \
 # Executing containers to the networks
 #tmux set-option remain-on-exit on
 tmux split-window "sudo docker exec -it client bash -c 'echo \"192.168.101.2 server\" >> /etc/hosts && cat /etc/hosts'"  && \
 tmux split-window "sudo docker exec -it server bash -c 'echo \"192.168.100.2 client\" >> /etc/hosts && cat /etc/hosts'" && \
-tmux split-window "sudo docker exec -it router bash -c 'ifconfig'"
+tmux split-window "sudo docker exec -it router bash -c 'cat /etc/hosts'"
