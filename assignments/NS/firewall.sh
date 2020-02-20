bb05b5196b72cef65e72bf4cd89c4ca0ee208cf1
amar.lakshya <axx945@cs.bham.ac.uk>
2020-02-20 23:45:30 +0000

    this  works

--- assignments/NS/firewall.sh
+++ assignments/NS/firewall.sh
@@ -26,21 +26,35 @@ sudo docker build  -t ubuntu-router router/ &&
 sudo docker build  -t ubuntu-client client/ &&
 sudo docker build  -t ubuntu-server server/ &&
 
+sudo docker network create --driver=bridge --subnet=192.168.100.0/24  client-net && \
+sudo docker network create --driver=bridge --subnet=192.168.101.0/24  server-net && \
 
-# Creating server-net and client-net networks
-sudo docker network create --driver=bridge --subnet=192.168.100.0/24 --ip-range=192.168.100.1/24  client-net && \
-sudo docker network create --driver=bridge --subnet=192.168.101.0/24 --ip-range=192.168.101.1/24  server-net && \
-
-# Attaching containers to the networks
-sudo docker run -itd --network=client-net --name=client --hostname=client ubuntu-client /bin/bash && \
-sudo docker run -itd --network=server-net --name=server --hostname=server ubuntu-server /bin/bash && \
-
-# Creating and attaching router container to both server-net and client-net networks
-sudo docker create -it --network=server-net  --name=router --hostname=router ubuntu-router /bin/bash && \
+sudo docker create -it --network=client-net --name=client --hostname=client ubuntu-client /bin/bash && \
+sudo docker create -it --network=server-net --name=server --hostname=server ubuntu-server /bin/bash && \
+sudo docker create -it --network=server-net --name=router --hostname=router ubuntu-router /bin/bash && \
 sudo docker network connect client-net router && \
 sudo docker start router && \
+sudo docker start client && \
+sudo docker start server && \
 
+# Creating server-net and client-net networks
+#sudo docker network create --driver=bridge --subnet=192.168.100.0/24 --ip-range=192.168.100.1/24  client-net && \
+#sudo docker network create --driver=bridge --subnet=192.168.101.0/24 --ip-range=192.168.101.1/24  server-net && \
+#
+# Attaching containers to the networks
+#sudo docker run -itd --network=client-net --name=client --hostname=client ubuntu-client /bin/bash && \
+#sudo docker run -itd --network=server-net --name=server --hostname=server ubuntu-server /bin/bash && \
+#sudo docker run -itd --network=server-net --name=router --hostname=router ubuntu-router /bin/bash && \
+#sudo docker network connect client-net router && \
+##sudo docker start router && \
+#
+
+# Creating and attaching router container to both server-net and client-net networks
+#sudo docker create -it --network=server-net  --name=router --hostname=router vrnetlab/vr-routeros:6.45.8 /bin/bash && \
+
+#sudo docker start router && \
+#
 # Executing containers to the networks
-tmux split-window "sudo docker exec -it  --privileged  router bash -c '/bin/bash'" && \
-tmux split-window "sudo docker exec -it client bash -c '. /etc/apache2/envvars && mkdir -p /var/run/apache2 && /bin/bash'" && \
-tmux split-window "sudo docker exec -it server bash -c '. /etc/apache2/envvars && mkdir -p /var/run/apache2 && /bin/bash'"
+tmux split-window "sudo docker exec -it  --privileged router bash -c '/bin/bash'" && \
+tmux split-window "sudo docker exec -it  --privileged client bash -c '. /etc/apache2/envvars && mkdir -p /var/run/apache2 && /bin/bash'" && \
+tmux split-window "sudo docker exec -it  --privileged server bash -c '. /etc/apache2/envvars && mkdir -p /var/run/apache2 && /bin/bash'"
