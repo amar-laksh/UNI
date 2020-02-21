31e0972a688eab216d9b549a8e1e9315fac7ca27
amar.lakshya <axx945@cs.bham.ac.uk>
2020-02-21 00:46:49 +0000

    fml

--- assignments/NS/firewall.sh
+++ assignments/NS/firewall.sh
@@ -37,10 +37,6 @@ sudo docker start router && \
 sudo docker start client && \
 sudo docker start server && \
 
-# Creating server-net and client-net networks
-#sudo docker network create --driver=bridge --subnet=192.168.100.0/24 --ip-range=192.168.100.1/24  client-net && \
-#sudo docker network create --driver=bridge --subnet=192.168.101.0/24 --ip-range=192.168.101.1/24  server-net && \
-#
 # Attaching containers to the networks
 #sudo docker run -itd --network=client-net --name=client --hostname=client ubuntu-client /bin/bash && \
 #sudo docker run -itd --network=server-net --name=server --hostname=server ubuntu-server /bin/bash && \
@@ -49,11 +45,7 @@ sudo docker start server && \
 ##sudo docker start router && \
 #
 
-# Creating and attaching router container to both server-net and client-net networks
-#sudo docker create -it --network=server-net  --name=router --hostname=router vrnetlab/vr-routeros:6.45.8 /bin/bash && \
 
-#sudo docker start router && \
-#
 # Executing containers to the networks
 tmux split-window "sudo docker exec -it  --privileged router bash -c '/bin/bash'" && \
 tmux split-window "sudo docker exec -it  --privileged client bash -c '. /etc/apache2/envvars && mkdir -p /var/run/apache2 && /bin/bash'" && \
