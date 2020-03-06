07a3b0bf0f41b70abe2cc6bb19ec162cfe8c3525
amar.lakshya <axx945@cs.bham.ac.uk>
2020-03-06 01:03:24 +0000

    added: reset script

--- assignments/NS/virtualbox/part3.sh
+++ assignments/NS/virtualbox/part3.sh
@@ -17,25 +17,23 @@
 #      REVISION:  ---
 #===============================================================================
 
-#set -o nounset                                  # Treat unset variables as an error
+set -o nounset                                  # Treat unset variables as an error
 MY_IP="192.168.101.2"
 
 # Flushing all existing chains
-sudo iptables -F INPUT
-sudo iptables -F FORWARD
-sudo iptables -F OUTPUT
+sudo iptables -F INPUT &&
+sudo iptables -F FORWARD &&
+sudo iptables -F OUTPUT &&
 
 # Setting default permit rules
-sudo iptables -P INPUT DROP
-sudo iptables -P OUTPUT ACCEPT
-#sudo iptables -P FORWARD DROP &&
+sudo iptables -P INPUT DROP &&
+sudo iptables -P OUTPUT ACCEPT &&
+sudo iptables -P FORWARD DROP &&
 
 # Allow unlimited traffic on loopback
-sudo iptables --append INPUT -i lo -j ACCEPT
-sudo iptables --append OUTPUT -o lo -j ACCEPT
+sudo iptables --append INPUT -i lo -j ACCEPT &&
+sudo iptables --append OUTPUT -o lo -j ACCEPT &&
 
 # Setting specific rules according to the part
-sudo iptables --append INPUT --protocol tcp --src 0/0 --sport 22 --dst $MY_IP --dport 22 -m state --state NEW,ESTABLISHED,RELATED --jump ACCEPT
-#sudo iptables --append OUTPUT --protocol tcp --src $MY_IP --sport 22 -m state --state NEW,ESTABLISHED --jump ACCEPT &&
-#sudo iptables --append OUTPUT --protocol udp --src $MY_IP --sport 22 -m state --state NEW,ESTABLISHED --jump ACCEPT &&
+sudo iptables --append INPUT --protocol tcp --dst $MY_IP --dport 22 -m state --state NEW,ESTABLISHED,RELATED --jump ACCEPT &&
 sudo iptables --append OUTPUT --jump ACCEPT
