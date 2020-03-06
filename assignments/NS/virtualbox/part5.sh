5ae1073f42f77f3956ac23910edc47183a405a09
amar.lakshya <axx945@cs.bham.ac.uk>
2020-03-06 03:02:49 +0000

    ADDED: part 5

--- assignments/NS/virtualbox/part5.sh
+++ assignments/NS/virtualbox/part5.sh
@@ -19,3 +19,28 @@
 
 set -o nounset                                  # Treat unset variables as an error
 
+SERVERNET="192.168.101.2/24"
+CLIENTNET="192.168.100.2/24"
+
+# Creating a separate logging chain for clean log drops
+sudo iptables -N logdrop &&
+sudo iptables -A logdrop -m limit --limit 1/s --limit-burst 100 -j LOG &&
+sudo iptables -A logdrop -j DROP &&
+
+# Flushing all existing chains
+sudo iptables -F INPUT &&
+sudo iptables -F FORWARD &&
+sudo iptables -F OUTPUT &&
+
+# Setting default permit rules
+sudo iptables -P INPUT ACCEPT &&
+sudo iptables -P OUTPUT ACCEPT &&
+sudo iptables -P FORWARD ACCEPT &&
+
+# Allow unlimited traffic on loopback
+sudo iptables --append INPUT -i lo -j ACCEPT &&
+sudo iptables --append OUTPUT -o lo -j ACCEPT &&
+
+# Deny all traffic except known addresses
+sudo iptables -A INPUT --protocol all ! --src $SERVERNET --jump DROP &&
+sudo iptables -A INPUT --protocol all ! --src $CLIENTNET --jump DROP
